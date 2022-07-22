e4b4f49f548a5d324d601db96178ffb1d69b4587
haiyang <reverocean@gmail.com>
2022-07-23 07:21:02 +0800

    implement validate new block logic

--- src/models/Blockchain.ts
+++ src/models/Blockchain.ts
@@ -1,8 +1,9 @@
 import {Block} from './Block'
+import * as CryptoJS from "crypto-js";
 
 class Blockchain {
   private static genesisBlock: Block = new Block(
-    0, '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7', 1465154705, 'my genesis block!!'
+    0, '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7', '', 1465154705, 'my genesis block!!'
   )
   private blockchain: Block[] = [Blockchain.genesisBlock]
 
@@ -10,10 +11,45 @@ class Blockchain {
     const previousBlock: Block = this.getLatestBlock();
     const nextIndex: number = previousBlock.index + 1
     const nextTimestamp: number = new Date().getTime() / 1000
-    return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData);
+    let hash = this.calculateHash(nextIndex, previousBlock.previousHash, nextTimestamp, blockData)
+    return new Block(nextIndex, hash, previousBlock.hash, nextTimestamp, blockData)
+  }
+
+  public isValidNewBlock(newBlock: Block, previousBlock: Block): boolean {
+    if (previousBlock.index + 1 !== newBlock.index) {
+      console.log('invalid index')
+      return false
+    }
+
+    if (previousBlock.hash !== newBlock.previousHash) {
+      console.log('invalid previous hash')
+      return false
+    }
+
+    if (this.calculateBlockHash(newBlock) !== newBlock.hash) {
+      console.log('invalid this block hash')
+      return false
+    }
+    return true
+  }
+
+  public isValidBlockStructure(newBlock: Block): boolean {
+    return typeof newBlock.index === 'number'
+      && typeof newBlock.hash === 'string'
+      && typeof newBlock.previousHash === 'string'
+      && typeof newBlock.timestamp === 'number'
+      && typeof newBlock.data === 'string'
+  }
+
+  private calculateHash(index: number, previousHash: string, timestamp: number, data: string): string {
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString()
   }
 
   private getLatestBlock(): Block {
     return this.blockchain[this.blockchain.length - 1];
+  }
+
+  private calculateBlockHash(newBlock: Block) {
+    return this.calculateHash(newBlock.index, newBlock.previousHash, newBlock.timestamp, newBlock.data)
   }
 }
